3300795da224dd29c2b595c6b059b63eef27270a
Lera Bonne <val.h1i2@icloud.com>
2026-05-12 12:33:39 +0300

    printFile fix

--- printFile.c
+++ printFile.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-int main(int argv, char* argc[])
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 0x20
+int main(int argc, char* argv[])
 {
-	char c;
-	FILE* myFile;
-	for(int i = 1; i <  argv; i++){
-	   myFile = fopen(argc[i], "r");
-	   if (!myFile)
+
+	int myFile;
+	char buff[BUFF_SIZE];
+	int debug;
+	for(int i = 1; i <  argc; i++)
+	{
+	   myFile = open(argv[i], O_RDONLY);
+	   if (myFile < 0)
 	   {
 		   printf("Error openning file!\n");
+		   continue;
 	   }
-	   while((c = fgetc(myFile)) != EOF )
+	   memset(buff, 0, BUFF_SIZE);
+	   while((debug = read(myFile, buff, BUFF_SIZE)) > 0)
 	   {
-	      putchar(c);
+		  //printf("%d\n",debug);
+	      write(STDOUT_FILENO, buff, debug);
        }
-	   fclose(myFile);
+       printf("\n");
+	   close(myFile);
    }
    return 0;
 }
