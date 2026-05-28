09154d2ff015241cfdad877594139a32845d33b7
Lera Bonne <val.h1i2@icloud.com>
2026-05-28 22:11:24 +0300

    print First ten code review WIP

--- printFirstTen.c
+++ printFirstTen.c
@@ -5,35 +5,9 @@
 #include <unistd.h>
 
 
-#define BUFF_SIZE 0x100
+#define BUFF_SIZE 0x10
 
 char buff[BUFF_SIZE];
-
-static int readIntoBuffer(int fd)
-{
-//	printf("readIntoBuffer\n");
-		int rdRes = 0;
-		int bytesRead = 0;
-		do
-		{
-			//bytesRead = 0;
-			if((rdRes = read(fd, buff + rdRes, BUFF_SIZE - bytesRead))>= 0)
-			{
-				bytesRead += rdRes;
-		//		printf("rdRes = %d\n", rdRes);
-	//			printf("bytesRead = %d\n",bytesRead );
-				
-			}
-			else 
-			{
-				perror("Error reading file! ");
-				return 0;
-			}
-	   }while(rdRes > 0);
-//	   printf("bytesRead = %d\n", bytesRead);
-//	   printf(" return rdRes = %d\n", rdRes);	
-	   return bytesRead;
-}
 
 static int printLine(void* buff, unsigned size)
 {
@@ -54,46 +28,63 @@ static int printLine(void* buff, unsigned size)
 	return 1;
 }
 
+static int printTenLines()
+{
+	int i;
+	static int lineCount = 0;
+	for(i = 0; i < BUFF_SIZE; i++)
+	{
+		if(buff[i] == '\n')
+		{
+			lineCount++;
+			if(lineCount == 10)
+			{
+				printLine(buff, i + 1);
+				return 1;
+			}
+		}
+	}
+	printLine(buff, BUFF_SIZE);
+	return 0;
+}
+
+static int readIntoBuffer(int fd)
+{
+		int rdRes = 0;
+		int bytesRead = 0;
+		do
+		{
+			if((rdRes = read(fd, buff + rdRes, BUFF_SIZE - bytesRead))>= 0)
+			{
+				bytesRead += rdRes;
+			}
+			else
+			{
+				perror("Error reading file! ");
+				return 0;
+			}
+	   }while(rdRes > 0);
+	   return 1;
+}
+
 int main(int argc, char* argv[])
 {
-
 	int myFile;
-	int lineCount = 0;
-    char* pBuff = buff;
-    myFile = open(argv[1], O_RDONLY);
+	myFile = open(argv[1], O_RDONLY);
     
-    if (myFile < 0)
-    {
+	if (myFile < 0)
+	{
 		perror("Error openning file! ");
 		return EXIT_FAILURE;
-    }
-    /*Todo add print buffer when \n is not found*/
-    while(readIntoBuffer(myFile))
-    {
-	//	printf ("%s\n", buff);
-		int i;
-		pBuff = buff;
-		for(i = 0; i < BUFF_SIZE; i++)
-		{
-		   if(pBuff[i] == '\n')
-		   {	
-				lineCount++;
-				if(lineCount == 10)
-				{
-					//pBuff+= (i + 1);
-					printLine(pBuff, i + 1);
-					goto end;
-				}
-		//		printLine(buff, BUFF_SIZE);
-	//			printf("lineCount = %d\n ", lineCount );
-				
-			}
-			
-		}
-		printLine(buff, BUFF_SIZE); 
-		
 	}
-	end:
+
+	while(readIntoBuffer(myFile))
+	{
+		if(printTenLines())
+		{
+			break;
+		}
+	}
 	close(myFile);
 	return 0;
 }
