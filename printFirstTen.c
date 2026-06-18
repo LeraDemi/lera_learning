39daa358bceffb31b4b1e8e52a3e8e6f274b370b
Lera Bonne <val.h1i2@icloud.com>
2026-06-18 19:10:02 +0300

    remove unused includes

--- printFirstTen.c
+++ printFirstTen.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include "common.h"
 
 static int printTenLines(const void* buff, size_t size, ssize_t *lineCount)
