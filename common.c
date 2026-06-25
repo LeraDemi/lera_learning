9df84d4bb31e8ed0d47d8d5c349f2d312c41ea48
Lera Bonne <val.h1i2@icloud.com>
2026-06-25 11:34:08 +0300

    commom.c common.h - Placed function headers in h files instesd of c

--- common.c
+++ common.c
@@ -2,15 +2,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/**
- * @brief Gets file descriptor and returns its size
- *
- * Function uses fstat to retrieve file size by descriptor
- *
- * @param fd file descriptor
- *
- * @return ssize_t returns the size upon success or -1 if an error accours.
- */
 ssize_t getFileSize(int fd)
 {
 	struct stat sStat;
@@ -21,19 +12,7 @@ ssize_t getFileSize(int fd)
 	}
 	return sStat.st_size;
 }
-/**
- * @brief Function copies bytes from file to a buffer
- *
- * Function calls read command to read bytes from file into the buffer.
- * Fuction checks for errors during file read
- *
- * @param fd File - descriptor
- * @param buff - buffer to copy file content to
- * @param size - size of file in bytes
- *
- * @return returns number of read bytes or 0 if error accurs
- *
- */
+
 int readIntoBuffer(int fd, void* buff, size_t size)
 {
 	ssize_t rdRes = 0;
@@ -53,14 +32,7 @@ int readIntoBuffer(int fd, void* buff, size_t size)
    while(rdRes > 0);
    return bytesRead;
 }
-/**
- * @brief This function prints to STDOUT all bytes in buffer
- *
- * This function calls write command to write bytes from buffer to output
- *
- * @param buff - Pionter to buffer to print
- * @param size - Num of bytes to print
- * */
+
 int printBuffer(const void* buff, size_t size)
 {
 	size_t written = 0;
--- common.h
+++ common.h
@@ -2,9 +2,39 @@
 #define _COMMON_H
 #include <stdio.h>
 
-
+/**
+ * @brief Gets file descriptor and returns its size
+ *
+ * Function uses fstat to retrieve file size by descriptor
+ *
+ * @param fd file descriptor
+ *
+ * @return ssize_t returns the size upon success or -1 if an error accours.
+ */
 ssize_t getFileSize(int fd);
+/**
+ * @brief Function copies bytes from file to a buffer
+ *
+ * Function calls read command to read bytes from file into the buffer.
+ * Fuction checks for errors during file read
+ *
+ * @param fd File - descriptor
+ * @param buff - buffer to copy file content to
+ * @param size - size of file in bytes
+ *
+ * @return returns number of read bytes or 0 if error accurs
+ *
+ */
 int readIntoBuffer(int fd, void* buff, size_t size);
+}
+/**
+ * @brief This function prints to STDOUT all bytes in buffer
+ *
+ * This function calls write command to write bytes from buffer to output
+ *
+ * @param buff - Pionter to buffer to print
+ * @param size - Num of bytes to print
+ * */
 int printBuffer(const void* buff, size_t size);
 
 #endif
